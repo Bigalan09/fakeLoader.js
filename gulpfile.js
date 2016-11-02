9248201be8950774b0d5fb42a6bb07d71d6915d3
Bigalan09 <bigalan09@gmail.com>
2016-11-02 10:40:05 +0000

    Added gulp file for minifying files.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+var rename = require("gulp-rename");
+
+gulp.task('default', ['minify:css', 'minify:js'], function() {});
+
+gulp.task('minify:css', function() {
+    return gulp.src('fakeLoader.css')
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minify:js', function() {
+    return gulp.src('fakeLoader.js')
+        .pipe(minify({
+            ext: {
+                src: '-debug.js',
+                min: '.js'
+            }
+        }))
+        .pipe(gulp.dest('dist/js'));
+});
